32021225650855e532d397f95886902dcc7befc2
Yashraj Kakkad <yashrajkakkad@gmail.com>
2020-05-09 11:59:32 +0530

    Add noise is now generate noise

--- noise.py
+++ noise.py
@@ -1,13 +1,14 @@
 import numpy as np 
 from scipy.signal import periodogram
 
-def add_noise(signal, N0, f_s):
+def generate_noise(signal, N0, f_s):
     N0_unit_power = 0.0004
     ns = len(signal)
     noise = np.random.normal(size=ns)
     noise *= np.sqrt(N0/N0_unit_power)
-    f, psd = periodogram(noise, f_s)
-    psd_av = np.mean(psd)
+    # f, psd = periodogram(noise, f_s)
+    # psd_av = np.mean(psd)
     # N0 = 2*psd_av
-    signal_with_noise = signal + noise
-    return signal_with_noise
+    return noise
+    # signal_with_noise = signal + noise
+    # return signal_with_noise
